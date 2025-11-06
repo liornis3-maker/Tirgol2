272773ff6489aff46479772f313b94881e1e4082
liornisanov1 <Lior@10.10.2.127>
2025-11-06 14:55:05 +0200

    import java.util.ArrayList; import java.util.Scanner;
    public class TestThreadCheckArray {
        public static void main(String[] args) {
            try (Scanner input = new Scanner(System.in)) {
                Thread thread1, thread2;
    
                // Ask user for the list size
                System.out.println("Enter ArrayList size");
                int num = input.nextInt();
    
                // Create an ArrayList with given size
                ArrayList<Integer> arraylist = new ArrayList<>(num);
    
                // Fill the ArrayList with numbers from user
                System.out.println("Enter numbers for array");
                for (int index = 0; index < num; index++)
                    arraylist.add(input.nextInt());
    
                // Ask user for the target number
                System.out.println("Enter number");
                num = input.nextInt();
    
                // Shared object between both threads
                SharedData sd = new SharedData(arraylist, num);
    
                // Create two threads that check the array
                thread1 = new Thread(new ThreadCheckArray(sd), "thread1");
                thread2 = new Thread(new ThreadCheckArray(sd), "thread2");
    
                // Start both threads
                thread1.start();
                thread2.start();
    
                // Wait until both threads finish
                try {
                    thread1.join();
                    thread2.join();
                } catch (InterruptedException e) {
                    e.printStackTrace();
                }
    
                // If number was not found
                if (!sd.getFlag()) {
                    System.out.println("Sorry");
                    return;
                }
    
                // If found, print the result and data
                System.out.println("Solution for b : " + sd.getB() + ", n =
    " + sd.getArray().size());
    
                // Print indexes
                System.out.print("I:    ");
                for (int index = 0; index < sd.getArray().size(); index++)
                    System.out.print(index + "    ");
                System.out.println();
    
                // Print array values
                System.out.print("A:    ");
                for (int index : sd.getArray()) {
                    System.out.print(index);
                    int counter = 5;
                    while (true) {
                        index = index / 10;
                        counter--;
                        if (index == 0)

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 
+ */
 public class SharedData 
 {
 	private ArrayList <Integer> array;
@@ -8,36 +11,61 @@ public class SharedData
 	private boolean flag;
 	private final int b;
 	
+	/** SharedData class holds the shared information used by multiple threads.
+	 * @param array
+	 * @param constarctor 
+	 */
 	public SharedData(ArrayList array, int b) {
 		this.array =array;
 		this.b = b;
 	}
 
+	/**  Boolean array representing which elements matched (true/false)
+	 * @return  winArray
+	 */ 
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
 
+	/** Sets the result boolean array after processing.
+	 * @param winArray the boolean array to set
+	 */
 	public void setWinArray(boolean [] winArray) 
 	{
 		this.winArray = winArray;
 	}
 
+	/**Returns the list of integers being processed.
+	 * @return array the ArrayList of integers
+	 */
 	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
 
+	/** Returns the target number to search for.
+	 * @return  b the target number
+	 */
 	public int getB() 
 	{
 		return b;
 	}
 
+	/**Returns the current flag value.
+     * Indicates whether the target number was found or not.
+	 * @return  true if the target number was found, false otherwise
+	 */
 	public boolean getFlag() 
 	{
 		return flag;
 	}
 
+	/**
+	 * Sets the flag value.
+     * Used by threads to update whether the target number was found.
+	 * @param flag
+	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
